1063bc066fa303a0adfb4cfb0010980fe0a3e5b1
satish <satishkonduru0225@gmail.com>
2024-11-22 07:56:52 +0530

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CourseComponent } from './components/course/course.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'course', //localhost:4200/course
+    component: CourseComponent,
+  },
+  {
+    path: 'courseDetails', //localhost:4200/courseDetails
+    component: CourseDetailsComponent,
+  },
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,12 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, CommonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  preserveWhitespaces: true,
 })
 export class HeaderComponent {}
