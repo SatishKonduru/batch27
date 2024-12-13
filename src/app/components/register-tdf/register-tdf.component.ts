02fe7be46d072bdfccb8878edb979142a5cf7a6c
satish <satishkonduru0225@gmail.com>
2024-12-13 07:57:27 +0530

    TDF Completed

--- src/app/components/register-tdf/register-tdf.component.ts
+++ src/app/components/register-tdf/register-tdf.component.ts
@@ -5,14 +5,23 @@ import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCalendar, MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-tdf',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatToolbarModule, MatDatepickerModule, MatCalendar],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatToolbarModule, MatDatepickerModule, MatCalendar,MatRadioModule, MatSelectModule,MatButtonModule, FormsModule],
   templateUrl: './register-tdf.component.html',
   styleUrl: './register-tdf.component.css'
 })
 export class RegisterTDFComponent {
 
+  onSubmit(form: NgForm ){
+    //  console.log(form.value)
+    //  console.log(form.controls.firstName.value)
+    console.log(form)
+  }
 }
