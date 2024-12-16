ac7ef66990e4cef9ac19dbd032512e4e675ffa17
satish <satishkonduru0225@gmail.com>
2024-12-16 08:22:44 +0530

    Forms Completed

--- src/app/components/register-rf/register-rf.component.ts
+++ src/app/components/register-rf/register-rf.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 
 @Component({
@@ -18,21 +18,32 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class RegisterRfComponent implements OnInit{
   registerForm : any = FormGroup
+  changeDetetor = inject(ChangeDetectorRef)
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      firstName: new FormControl(null),
+      firstName: new FormControl(null, Validators.required),
       lastName : new FormControl(null),
-      email: new FormControl(null),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       phone: new FormControl(null),
       dob: new FormControl(null),
-      gender: new FormControl(null),
-      address1: new FormControl(null),
-      address2: new FormControl(null),
-      country: new FormControl(null),
-      city: new FormControl(null),
-      region: new FormControl(null),
-      postalCode: new FormControl(null)
+      gender: new FormControl('Male'),
+      address: new FormGroup({
+                address1: new FormControl(null),
+                address2: new FormControl(null),
+                country: new FormControl(null),
+                city: new FormControl(null),
+                region: new FormControl(null),
+                postalCode: new FormControl(null)
+      }),
+      skills : new FormArray([]),
+      experience: new FormArray([
+        new FormGroup({
+          cName: new FormControl(null),
+          location: new FormControl(null)
+        })
+      ])
+
     })
   }
 
@@ -42,4 +53,22 @@ export class RegisterRfComponent implements OnInit{
     console.log("Form Data: ", formData)
   }
 
+  addSkill(){
+    (<FormArray>this.registerForm.controls.skills).push(new FormControl(null))
+    this.changeDetetor.detectChanges()
+  }
+
+  deleteSkill(index: any){
+    const control = <FormArray>this.registerForm.controls.skills
+    control.removeAt(index)
+  }
+
+
+  addExperience(){
+    (<FormArray>this.registerForm.controls.experience).push(new FormGroup({
+      cName: new FormControl(null),
+      location: new FormControl(null)
+    }))
+    this.changeDetetor.detectChanges()
+  }
 }
